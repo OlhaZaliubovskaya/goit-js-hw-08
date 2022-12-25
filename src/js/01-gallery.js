187a7ee85151a79336af6efede9963e8a9074e10
Olha Zaliubovskaya <110026359+OlhaZaliubovskaya@users.noreply.github.com>
2022-12-25 19:45:14 +0200

    SimpleLightbox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,29 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+
+const gallery = document.querySelector(".gallery");
+
+gallery.insertAdjacentHTML(
+  'afterbegin',
+  galleryItems
+    .map(
+      galleryItem =>
+        `<a class="gallery__item" href="${galleryItem.original}">
+        <img class="gallery__image"
+        src="${galleryItem.preview}" 
+        alt= "${galleryItem.description}"></a>`
+    )
+    .join('')
+);
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,43 @@
+import throttle from 'lodash.throttle';
 
+// знаходімо форму 
+const feedbackForm = document.querySelector('.feedback-form');
+
+// додаємо слухача 
+feedbackForm.addEventListener('input', throttle(localData, 500));
+
+// знаходімо вхідні дані
+const email = document.querySelector('[name="email"]');
+const message = document.querySelector('[name="message"]');
+
+//  встановити дані з вхідних даних у локальне сховище
+function localData() {
+  const formData = {
+    email: email.value,
+    message: message.value,
+  };
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}
+
+// функція для отримання вхідних даних із локального сховища
+function getLocalData() {
+  let localData = JSON.parse(localStorage.getItem('feedback-form-state'));
+  if (localData !== null) {
+    email.value = localData.email;
+    message.value = localData.message;
+  }
+}
+
+// запускаємо
+getLocalData();
+
+// запускаємо та надсілаємо
+feedbackForm.addEventListener('submit', submitData);
+
+// запустити функцію під час надсилання та вхідних даних журналу консолі
+function submitData(e) {
+  e.preventDefault();
+  this.reset();
+  console.log(localStorage.getItem('feedback-form-state'));
+  localStorage.removeItem('feedback-form-state');
+}
