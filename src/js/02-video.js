0693ce89686d272990f633646bf439bf32142a12
Olha Zaliubovskaya <110026359+OlhaZaliubovskaya@users.noreply.github.com>
2023-01-05 13:10:50 +0200

    02-video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,15 +1,30 @@
 import Player from '@vimeo/player';
-import throttle from 'lodash.throttle';
+import throttle from "lodash.throttle";
 
-const video = document.querySelector("#vimeo-player");
+const video = document.querySelector('#vimeo-player');
 
 const player = new Player(video);
 
-player.on(
-  "timeupdate",
-  throttle(function playTime(e) {
-    localStorage.setItem("vid-time", e.seconds)
-  }, 1000),
+player.on('timeupdate', throttle(setWatchingTime, 1000));
+
+function setWatchingTime(e) {
+  localStorage.setItem('videoplayer-current-time', e.seconds);
+}
+const getContentOfLocalStorage = (
+  localStorage.getItem('videoplayer-current-time') || 0
 );
 
-player.setCurrentTime(localStorage.getItem('vid-time')) ?? 0;
+player.setCurrentTime(getContentOfLocalStorage);
+
+console.log(Player);
+
+
+
+// player.on(
+//   "timeupdate",
+//   throttle(function playTime(e) {
+//     localStorage.setItem("vid-time", e.seconds)
+//   }, 1000),
+// );
+
+// player.setCurrentTime(localStorage.getItem('vid-time')) ?? 0;
